3ae2dde2e0041bd58e074fb8d8e8029e51562d16
mbullxyz <mbullxyz@gmail.com>
2024-08-01 12:51:22 +0700

    Update ano.bat

--- ano.bat
+++ ano.bat
@@ -1,20 +1,6 @@
 @echo off
-del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user mbullxyz narutopein /add >nul
-net localgroup administrators mbullxyz /add >nul
-net user mbullxyz /active:yes >nul
-net user installer /delete
-diskperf -Y >nul
-sc config Audiosrv start= auto >nul
-sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant mbullxyz:F >nul
-ICACLS C:\Windows\installer /grant mbullxyz:F >nul
-echo Successfully installed! If RDP is dead, rebuild again.
-echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: mbullxyz
-echo Password: narutopein
+echo Username: runneradmin
+echo Password: Qwe123!@#
 echo You can login now
 ping -n 10 127.0.0.1 >nul
 echo RDP CREATION SUCCESSFULL!
